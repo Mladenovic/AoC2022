95b209384b01a59bc9c4f63e2b2b04fb882ea336
Stevan Mladenovic <mladenovic@protonmail.com>
2022-12-04 10:21:52 +0100

    day4

---
+++ AoC/Day4.fs
@@ -0,0 +1,48 @@
+﻿module Day4
+open System.IO
+
+type Range = {
+    Low: int
+    High: int
+}
+
+type Assignment = {
+    FirstElf: Range
+    SecondElf: Range
+}
+
+let parseRange (range: string) =
+    let ar = range.Split "-"
+    { Low = int ar.[0]; High = int ar.[1] }
+    
+let parseAssignment (assignment: string) = 
+    let ar = assignment.Split ","
+    { FirstElf = parseRange ar.[0]; SecondElf = parseRange ar.[1] }
+    
+let input =
+    File.ReadAllLines "input/day4.txt"
+    |> Array.map parseAssignment
+    
+let isOneAssignmentCoveredByOther (assignment: Assignment) =
+    assignment.FirstElf.Low <= assignment.SecondElf.Low
+    && assignment.FirstElf.High >= assignment.SecondElf.High
+    || assignment.FirstElf.Low >= assignment.SecondElf.Low
+    && assignment.FirstElf.High <= assignment.SecondElf.High
+       
+let day4Part1Solution =
+    input
+    |> Array.map isOneAssignmentCoveredByOther
+    |> Array.map (fun x -> if x then 1 else 0)
+    |> Array.sum
+    
+let areAssignmentsOverlapping (assignment: Assignment) =
+    assignment.FirstElf.Low >= assignment.SecondElf.Low && assignment.FirstElf.Low <= assignment.SecondElf.High
+    || assignment.SecondElf.Low >= assignment.FirstElf.Low && assignment.SecondElf.Low <= assignment.FirstElf.High
+    || assignment.FirstElf.High >= assignment.SecondElf.Low && assignment.FirstElf.High <= assignment.SecondElf.High
+    || assignment.SecondElf.High >= assignment.FirstElf.Low && assignment.SecondElf.High <= assignment.FirstElf.High
+    
+let day4Part2Solution =
+    input
+    |> Array.map areAssignmentsOverlapping
+    |> Array.map (fun x -> if x then 1 else 0)
+    |> Array.sum
--- AoC/Program.fs
+++ AoC/Program.fs
@@ -1,17 +1,20 @@
 ﻿open Day1
 open Day2
 open Day3
+open Day4
 
 
 let printSolution day part solution =
     printf $"Day %i{day} part %i{part} solution is %i{solution}\n\n"
     
-printSolution 1 1 day1SolutionPart1
-printSolution 1 2 day1SolutionPart2
-
-printSolution 2 1 day2Part1Solution
-printSolution 2 2 day2Part2Solution
-
-printSolution 3 1 day3Part1Solution
-printSolution 3 2 day3Part2Solution
+// printSolution 1 1 day1SolutionPart1
+// printSolution 1 2 day1SolutionPart2
+//
+// printSolution 2 1 day2Part1Solution
+// printSolution 2 2 day2Part2Solution
+//
+// printSolution 3 1 day3Part1Solution
+// printSolution 3 2 day3Part2Solution
     
+printSolution 4 1 day4Part1Solution
+printSolution 4 2 day4Part2Solution
